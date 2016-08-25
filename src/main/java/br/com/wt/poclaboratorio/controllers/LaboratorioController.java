ec0a5b52b829ef6d96056ea60c5c1e7fc0298a30
Willian Teophilo <willian@vdmax3d.com.br>
2016-08-25 14:28:58 -0300

    refactor

--- src/main/java/br/com/wt/poclaboratorio/controllers/LaboratorioController.java
+++ src/main/java/br/com/wt/poclaboratorio/controllers/LaboratorioController.java
@@ -21,51 +21,72 @@ import br.com.wt.poclaboratorio.modelo.LaboratorioRepository;
 @RestController
 @RequestMapping("/laboratorio")
 public class LaboratorioController {
-	
+
 	@Autowired
 	private LaboratorioRepository laboratorioRepository;
-	
-	@RequestMapping(value = "/",method = RequestMethod.POST ,produces = "application/json")
-	public ResponseEntity<Void> add(@RequestBody Laboratorio laboratorio ,UriComponentsBuilder ucBuilder){
-		System.out.println("Criado um nova data ");
+
+	@RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<Void> add(@RequestBody Laboratorio laboratorio, UriComponentsBuilder ucBuilder) {
 		HttpHeaders headers = new HttpHeaders();
-		try{
+		try {
 			laboratorioRepository.save(laboratorio);
 			headers.setLocation(ucBuilder.path("/{id}").buildAndExpand(laboratorio.getId()).toUri());
-			return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
-		}catch(RuntimeErrorException e){
+			return new ResponseEntity<Void>(headers, HttpStatus.OK);
+		} catch (RuntimeErrorException e) {
 			System.out.println(e.getMessage());
-			return new ResponseEntity<Void>(headers, HttpStatus.PRECONDITION_FAILED);
+			return new ResponseEntity<Void>(headers, HttpStatus.NOT_ACCEPTABLE);
 		}
-  
+
 	}
-	
-	@RequestMapping(value = "/lista", method = RequestMethod.GET,  produces = "application/json")
-	public List<Laboratorio> lista() {
+
+	@RequestMapping(value = "/lista", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<List<Laboratorio>> lista() {
 		List<Laboratorio> laboratorios = (List<Laboratorio>) laboratorioRepository.findAll();
-		return laboratorios;
+		if (laboratorios == null) {
+			return new ResponseEntity<>(laboratorios, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(laboratorios, HttpStatus.FOUND);
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
-	public Laboratorio getLaboratorio(@PathVariable Long id) {
+	public ResponseEntity<Laboratorio> getLaboratorio(@PathVariable Long id) {
 		Laboratorio laboratorio = laboratorioRepository.findOne(id);
-		return laboratorio;
+		if (laboratorio == null) {
+			return new ResponseEntity<>(laboratorio, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(laboratorio, HttpStatus.FOUND);
 	}
-	
-	@RequestMapping(value = "/doador/update",method = RequestMethod.PUT ,produces = "application/json")
-	public ResponseEntity<Void> update(@PathVariable long id,@RequestBody Laboratorio laboratorio,UriComponentsBuilder ucBuilder){
-		
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = "application/json")
+	public ResponseEntity<Void> update(@PathVariable long id, @RequestBody Laboratorio laboratorio,
+			UriComponentsBuilder ucBuilder) {
+		HttpHeaders headers = new HttpHeaders();
 		Laboratorio laboratorioBD = laboratorioRepository.findOne(id);
+
+		if (laboratorioBD == null) {
+			return new ResponseEntity<Void>(headers, HttpStatus.NOT_FOUND);
+		}
+
 		laboratorioBD.setCep(laboratorio.getCep());
 		laboratorioBD.setEmail(laboratorio.getEmail());
 		laboratorioBD.setEndereco(laboratorio.getEndereco());
 		laboratorioBD.setNome(laboratorio.getNome());
-		
+
 		laboratorioRepository.save(laboratorioBD);
-		HttpHeaders headers = new HttpHeaders();
 		headers.setLocation(ucBuilder.path("/{id}").buildAndExpand(laboratorio.getId()).toUri());
-        return new ResponseEntity<Void>(headers, HttpStatus.OK);
+		return new ResponseEntity<Void>(headers, HttpStatus.OK);
+
 	}
-	
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
+	public ResponseEntity<Void> deletar(@PathVariable Long id) {
+		Laboratorio laboratorio = laboratorioRepository.findOne(id);
+		if (laboratorio == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		laboratorioRepository.delete(laboratorio);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 
 }
